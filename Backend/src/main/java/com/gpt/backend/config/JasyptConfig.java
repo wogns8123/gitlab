ad4db43e4043368736fe1e506f7f813473fb2fad
박준수 <qwnstn@naver.com>
2023-04-25 17:07:49 +0900

    백엔드 세팅중, 환경변수 진행중

---
+++ Backend/src/main/java/com/gpt/backend/config/JasyptConfig.java
@@ -0,0 +1,38 @@
+package com.gpt.backend.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String PASSWORD;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(PASSWORD);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        log.info("Jasypt Config Completed.");
+
+        return encryptor;
+    }
+
+}
