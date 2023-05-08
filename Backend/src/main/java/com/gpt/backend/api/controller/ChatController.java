33e92bf8c7c890017c043882a8a7e0d382ea39ce
박준수 <qwnstn@naver.com>
2023-05-08 14:00:44 +0900

    ChatController로 이름 변경

---
+++ Backend/src/main/java/com/gpt/backend/api/controller/ChatController.java
@@ -0,0 +1,17 @@
+package com.gpt.backend.api.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+
+}
