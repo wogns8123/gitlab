8fc550c3fb69709d053274ea1c53ec2c5e192cb7
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-05-10 15:47:41 +0900

    logging

--- Backend/src/main/java/com/gpt/backend/api/service/GpuService.java
+++ Backend/src/main/java/com/gpt/backend/api/service/GpuService.java
@@ -1,5 +1,6 @@
 package com.gpt.backend.api.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class GpuService {
 
     private static final String GPU_API_URL = System.getenv("GPU_API_URL");
@@ -17,6 +19,7 @@ public class GpuService {
         HttpHeaders header = new HttpHeaders();
         header.setContentType(MediaType.APPLICATION_JSON);
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        log.info(GPU_API_URL);
         requestBody.add("text", text);
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, header);
         ResponseEntity<String> response = restTemplate.exchange(GPU_API_URL, HttpMethod.POST, request, String.class);
