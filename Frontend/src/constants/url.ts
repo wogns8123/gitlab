3603d9372ab739b2b839ce776b6e1bf8ef983099
minsu_kim <minsu8252@naver.com>
2023-05-17 17:25:36 +0900

    url 수정

--- Frontend/src/constants/url.ts
+++ Frontend/src/constants/url.ts
@@ -1,6 +1,6 @@
 const URLS = {
-  API: "http://localhost:8080",
-  // API: "https://customgptchat.com",
+  // API: "http://localhost:8080",
+  API: "https://customgptchat.com",
 };
 
 export default URLS;
