344299e93705d54ccb57454ff474dda0d7562d3c
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-05-18 10:13:35 +0900

    번역제거

--- Backend/src/main/java/com/gpt/backend/api/service/ChatService.java
+++ Backend/src/main/java/com/gpt/backend/api/service/ChatService.java
@@ -104,9 +104,11 @@ public class ChatService {
             //TODO Req에 채팅내역저장
         }
 
-        String answer = papagoTranslateService.translate("ko", "en", dto.getChat());
-        answer = gpuService.calculate(answer);
-        answer = papagoTranslateService.translate("en", "ko", answer);
+//        String answer = papagoTranslateService.translate("ko", "en", dto.getChat());
+//        answer = gpuService.calculate(answer);
+        String answer = gpuService.calculate(dto.getChat());
+
+//        answer = papagoTranslateService.translate("en", "ko", answer);
         Title title = titleRepository.findById(dto.getTitleId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 제목이 없습니다."));
         reqRepository.save(Req.builder()
